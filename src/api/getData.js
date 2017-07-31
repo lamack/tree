48871ad082f3938a66e4089bbe19efe50c45de6f
lamack <shopex@administratordeMac-mini-2.local>
2017-07-31 16:38:05 +0800

    getData

--- src/api/getData.js
+++ src/api/getData.js
@@ -13,8 +13,91 @@ export const hello = data => fetch('/Index/index/hello', data, 'POST');
 export const login = data => fetch('/user/user/login', data, 'POST');
 
 /**
+ * 会员接口
+ */
+
+// 会员列表
+export const memberList = data => fetch('/member/menber/index', data);
+
+// 会员获得纪录
+export const memberRecode = data => fetch('/member/menber/recode', data);
+
+// 会员信息
+export const memberInfo = data => fetch('/member/menber/info', data);
+
+
+// 灾害接口
+export const disaster = data => fetch('/disaster/disaster/index', data);
+// 灾害设置接口
+export const disasterSet = data => fetch('/disaster/disaster/disaster_setting', data,'POST')	;
+
+
+// 道具列表
+export const prop = data => fetch('/prop/prop/index', data);
+// 道具设置
+export const propSet = data => fetch('/prop/prop/pros_setting', data,'POST');
+
+// 转盘
+export const wheel = data => fetch('/wheel/wheel/reward', data);
+// 果实
+export const furit = data => fetch('/furit/furit/reward', data);
+// 转盘奖品设置
+export const wheelSet = data => fetch('/wheel/wheel/setting', data,'POST');
+// 果实设置
+export const furitSet = data => fetch('/furit/furit/furit_setting', data,'POST');
+// 获奖用户
+export const rewardUser = data => fetch('/reward/reward/user', data);
+// 获取用户详情
+export const rewardDetial = data => fetch('/reward/reward/userdetail', data);
+
+
+//竞答题库
+export const vie = data => fetch('/vie/vie/index', data);
+//竞答设置接口
+export const vieSet = data => fetch('/vie/vie/setting', data,'POST');
+
+
+//获得分享接口
+export const share = data => fetch('/share/share/index', data);
+//获得zan
+export const shareZan = data => fetch('/share/share/index', data);
+//zan设置
+export const zanSet = data => fetch('/share/share/zan_setting', data,'POST');
+//绿值互送设置
+export const shareGreen = data => fetch('/share/share/gift_green', data,'POST');
+//绿苗赠送设置
+export const shareTree = data => fetch('/share/share/gift_trees', data,'POST');
+
+
+//后台用户
+export const user = data => fetch('/user/user/index', data);
+//后台获得用户
+export const userInfo = data => fetch('/user/user/userinfo', data);
+//更新用户
+export const userUpdate = data => fetch('/user/user/uppw', data,'POST');
+//更新用户密码
+export const userUppw = data => fetch('/user/user/userupdate', data,'POST');
+//添加用户
+export const userAdd = data => fetch('/user/user/add', data,'POST');
+//添加角色
+export const userRoleadd= data => fetch('/user/user/roleadd', data,'POST');
+//获得角色权限
+export const userRole= data => fetch('/user/user/role', data);
+
+
+//任务列表
+export const adv = data => fetch('/task/task/userupdate', data);
+//任务设置
+export const taskSet = data => fetch('/task/task/task_setting', data,'POST');
+
+
+//获得公告
+export const adv = data => fetch('/adv/adv/announcement', data);
+
+/**
  * 退出
  */
+
 
 export const signout = () => fetch('/admin/singout');
 
