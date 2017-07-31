205ec50fd0f367d8aa3f5e5fa27a6b27c97e12c9
lamack <shopex@administratordeMac-mini-2.local>
2017-07-31 15:43:53 +0800

    。。

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -3,7 +3,7 @@
 		<el-row style="height: 100%;">
 	  		<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
 				<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
-					<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+					<!-- <el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item> -->
 					<el-submenu index="2">
 						<template slot="title"><router-link to="memberManage"><i class="el-icon-document"></i>会员管理</router-link></template>
 						<el-menu-item index="checkMemberEditor">查看编辑</el-menu-item>
